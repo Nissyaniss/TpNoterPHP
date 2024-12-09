cffb1024f3eab1f07baeedb5300538ddfa75d249
Yaniss Lasbordes <yaniss.lasbordes@gmail.com>
2024-12-09 10:25:10 +0100

    more shitier shitier shitier shitier shit

---
+++ src/view/user/error_connection.php
@@ -0,0 +1,2 @@
+<div class="alert alert-error" role="alert">Erreur nous n'avons pas reussi a vous connecter</div>
+<a href="?c=home" class="btn btn-primary">Retour à l'accueil</a>
---
+++ src/view/user/incription_reussite.php
@@ -0,0 +1,2 @@
+<div class="alert alert-success" role="alert">Votre compte a bien ete creer</div>
+<a href="?c=home" class="btn btn-primary">Retour à l'accueil</a>
