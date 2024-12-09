60dfcd24f17ce300e2cf8f9406ec0f42b1351126
Nathan Berlureau <nathan.berlureau@gmail.com>
2024-12-09 09:51:27 +0100

    Ajout connexion.php dans vue

---
+++ src/vue/user/connexion.php
@@ -0,0 +1,9 @@
+<form action="index.php?action=login" method="post">
+    <label for="email">Email :</label>
+    <input type="email" name="email" id="email" required>
+
+    <label for="password">Mot de passe :</label>
+    <input type="password" name="password" id="password" required>
+
+    <button type="submit">Se connecter</button>
+</form>
