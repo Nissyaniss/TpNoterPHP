2ac5bdfbcd6907bc0366731bef68dfd422d291e0
Yaniss Lasbordes <yaniss.lasbordes@gmail.com>
2024-12-09 10:04:53 +0100

    more shitier shitier shit

--- index.php
+++ index.php
@@ -7,31 +7,31 @@ $route = isset($_GET['c']) ? $_GET['c'] : 'home';
 $id = isset($_GET['id']) ? $_GET['id'] : 0;
 
 switch ($route) {
-    case 'home':
-        require_once(__DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'Controller' . DIRECTORY_SEPARATOR . 'homeController.php');
-        break;
+	case 'home':
+		require_once(__DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'controller' . DIRECTORY_SEPARATOR . 'HomeController.php');
+		break;
 
-    case 'inscription':
-        $userController = new UserController();
-        $userController->inscription();
-        break;
+	case 'inscription':
+		$userController = new UserController();
+		$userController->inscription();
+		break;
 
-    case 'inscrire':
-        $userController = new UserController();
-        $userController->enregistrer($pdo);
-        break;
+	case 'inscrire':
+		$userController = new UserController();
+		$userController->enregistrer($pdo);
+		break;
 
-    case 'connexion':
-        $userController = new UserController();
-        $userController->connexion();
-        break;
+	case 'connexion':
+		$userController = new UserController();
+		$userController->connexion();
+		break;
 
-    case 'connecter':
-        $userController = new UserController();
-        $userController->verify_connection($pdo);
-        break;
+	case 'connecter':
+		$userController = new UserController();
+		$userController->verify_connection($pdo);
+		break;
 
-    default:
-        echo "Erreur : route non définie.";
-        break;
+	default:
+		echo "Erreur : route non définie.";
+		break;
 }
